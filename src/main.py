75947af91715f3111dd92cf3cf0e88e92ec3663b
burak <karadumanburak000@gmail.com>
2024-01-15 17:02:42 +0300

    - Models added

--- src/main.py
+++ src/main.py
@@ -5,16 +5,38 @@ from src.api.v1.api import api_router
 
 from src.core.event_handler import start_app_handler, stop_app_handler
 
-#tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-mul-en")
-
 app = FastAPI(title=settings.PROJECT_NAME)
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
 app.add_event_handler("startup", start_app_handler(
     app,
-    ["/code/src/ml_model/opus-mt-mul-en.pkl"],
-    ["Helsinki-NLP/opus-mt-mul-en"]
+    [
+        "/code/src/ml_model/en-ar/model-opus-mt.pkl",
+        "/code/src/ml_model/en-az/model-opus-mt.pkl",
+        "/code/src/ml_model/en-de/model-opus-mt.pkl",
+        "/code/src/ml_model/en-es/model-opus-mt.pkl",
+        "/code/src/ml_model/en-fr/model-opus-mt.pkl",
+        "/code/src/ml_model/en-hi/model-opus-mt.pkl",
+        "/code/src/ml_model/en-it/model-opus-mt.pkl",
+        "/code/src/ml_model/en-ru/model-opus-mt.pkl",
+        "/code/src/ml_model/en-tr/model-opus-mt.pkl",
+        "/code/src/ml_model/en-zh/model-opus-mt.pkl",
+        "/code/src/ml_model/mul-en/model-opus-mt.pkl",
+     ],
+    [
+        "Helsinki-NLP/opus-mt-en-ar",
+        "Helsinki-NLP/opus-mt-en-az",
+        "Helsinki-NLP/opus-mt-en-de",
+        "Helsinki-NLP/opus-mt-en-es",
+        "Helsinki-NLP/opus-mt-en-fr",
+        "Helsinki-NLP/opus-mt-en-hi",
+        "Helsinki-NLP/opus-mt-en-it",
+        "Helsinki-NLP/opus-mt-en-ru",
+        "Helsinki-NLP/opus-tatoeba-en-tr",
+        "Helsinki-NLP/opus-mt-en-zh",
+        "Helsinki-NLP/opus-mt-mul-en",
+    ]
     ))
 
 app.add_event_handler("shutdown", stop_app_handler(app))
@@ -23,13 +45,3 @@ app.add_event_handler("shutdown", stop_app_handler(app))
 @app.get("/")
 def root():
     return {"message": "Hello World"}
-
-'''@app.get("/translate")
-def translateFromMultToEn(input):
-    print(input)
-    print(type(tokenizer))
-    model = pickle.load(open('src/ml_model/opus-mt-mul-en/model-opus-mt-mul-en.pkl', 'rb'))
-
-    input_ids = tokenizer(input, return_tensors='pt').input_ids
-    outputs = model.generate(input_ids=input_ids, num_beams=5, num_return_sequences=1)
-    return tokenizer.batch_decode(outputs, skip_special_tokens=True)'''
